f721285fa2aa3e7b13f0ef6aa080a7c15fbc46ef
Tessa Kelly <tessakkelly@gmail.com>
2015-04-29 16:24:19 -0700

    Remove extraneous build function from smallestMultiple

--- smallestMultiple.js
+++ smallestMultiple.js
@@ -1,8 +1,8 @@
 var primes = require('./sieveOfErastothanes.js');
-var _ = require('underscore');
+var Range = require('./range.js');
 
 var smallestMultiple = function(topNumber) {
-  var range = buildRange(2, topNumber);
+  var range = Range(2, topNumber);
   var mult = 1;
   for(var i = 0; i < range.length; i++) {
     for(var k = i + 1; k < range.length; k++) {
@@ -16,17 +16,5 @@ var smallestMultiple = function(topNumber) {
   });
 
 };
-
-var buildRange = function(start, topNumber) {
-  var rangeNums = [];
-  if(topNumber === undefined) {
-    topNumber = start;
-    start = 1;
-  }
-  for(var i = start; i <= topNumber; i++) {
-    rangeNums.push(i);
-  }
-  return rangeNums;
-}
 
 console.log(smallestMultiple(20));
