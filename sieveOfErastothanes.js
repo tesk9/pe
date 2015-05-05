c546319a5bd93e1b10037ece75c02fac02872e49
Tessa Kelly <tessakkelly@gmail.com>
2015-05-05 13:37:17 -0700

    rewrite sieveOfErastothanes so that it's actually a sieve (oops)

--- sieveOfErastothanes.js
+++ sieveOfErastothanes.js
@@ -1,21 +1,34 @@
+var Range = require('./range.js');
+
 var buildSieve = function(topNumber, primes) {
-  primes = primes || [3];
-  var num = primes[primes.length - 1] + 1;
-  
-  var isPrime = true;
-  for(; num < topNumber; num++) {
-    isPrime = true;
-    for(var i = 0; i < primes.length; i++) {  
-      if(!(num % primes[i])) {
-        isPrime = false;
-      }
-    }
-    if(isPrime) {
-      primes.push(num);
+  primes = primes || [2];
+
+  // Create range
+  var range = Range(primes[primes.length - 1], topNumber);
+
+  // Create range object
+  var rangeObj = {};
+  for(var i = 0; i < primes.length; i++) {
+    rangeObj[primes[i]] = 1;
+  }
+  for(var i = 0; i < range.length; i++) {
+    rangeObj[range[i]] = 1;
+  }
+
+  // Iterate over primes
+  var multiple, i;
+  for(var prime in rangeObj) {
+    i = 2;
+    multiple = prime;
+
+    while(multiple <= topNumber) {
+      multiple = prime * i;
+      delete rangeObj[multiple];
+      i++;
     }
   }
 
-  return primes;
+  return Object.keys(rangeObj);
 };
 
 module.exports = buildSieve;
