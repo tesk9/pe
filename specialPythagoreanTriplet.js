61a8b4a37fa167c0d6661cc29cc998c16feadeb9
Tessa Kelly <tessakkelly@gmail.com>
2015-05-04 19:31:47 -0700

    solve specialPythagoreanTriplet

---
+++ specialPythagoreanTriplet.js
@@ -0,0 +1,16 @@
+// There exists exactly one set of a,b,c such that a^2 + b^2 = c^2 and a + b + c = 1000
+
+var specialPythagoreanTriplet = function() {
+  var c;
+  for(var a = 1; a < 1000; a++) {
+    for(var b = 1; b < 1000; b++) {
+      c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+      if(a + b + c === 1000) {
+        console.log('num found');
+        return a * b * c;
+      }
+    }
+  }
+}
+
+console.log(specialPythagoreanTriplet());
