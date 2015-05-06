1081fe05b895f9f563f630982aeb69aff53547a6
Tessa Kelly <tessakkelly@gmail.com>
2015-05-05 17:23:20 -0700

    update sumPrimes to reflect sieve returning array of strings rather than array of ints

--- sumPrimes.js
+++ sumPrimes.js
@@ -3,8 +3,8 @@ var sieve = new require('./sieveOfErastothanes.js');
 var sumPrimes = function(topNum) {
   var primesArr = sieve(topNum);
   return primesArr.reduce(function(a,b) {
-    return a + b;
+    return Number(a) + Number(b);
   });
 };
 
-console.log(sumPrimes(2000000000));
+console.log(sumPrimes(2000000));
