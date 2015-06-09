b11eb80fa9a2c2578633a91882db290068853d0f
Tessa Kelly <tessakkelly@gmail.com>
2015-06-09 12:34:58 -0700

    solve countingSundays (fixed dictionary which had days in august and september switched oops I totally know the months don't judge me thanks)

--- countingSundays.js
+++ countingSundays.js
@@ -13,44 +13,76 @@
   How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
 */
 
-// How many Sundays were there between 1 Jan 1901 and 31 Dec 2000?
+var isLeapYear = function(year) {
+  if(!(year % 4)) {
+    if(!(year % 100)) {
+      return !(year % 400) ? true : false;
+    }
+    return true;
+  }
+  return false;
+}
 
-// How many of these Sundays fell on the first of the month?
-
-var thirtyOne = function(year) {
+var thirtyOne = function() {
   return 31;
 };
 
-var thirty = function(year) {
+var thirty = function() {
   return 30;
+};
+
+var daysInYear = function(year) {
+  return isLeapYear(year) ? 366 : 365;
 };
 
 var monthToDays = {
   1: thirtyOne,
   2: function(year) {
-    if(!(year % 4)) {
-      if(!(year % 100)) {
-        return !(year % 400) ? 29 : 28;
-      }
-      return 29;
-    }
-    return 28;
+    return isLeapYear(year) ? 29 : 28;
   },
   3: thirtyOne,
   4: thirty,
   5: thirtyOne,
   6: thirty,
   7: thirtyOne,
-  8: thirty,
-  9: thirtyOne,
+  8: thirtyOne,
+  9: thirty,
   10: thirtyOne,
   11: thirty,
   12: thirtyOne
 };
 
-console.log(monthToDays['2'](1) === 28);
-console.log(monthToDays['2'](4) === 29);
-console.log(monthToDays['2'](100) === 28);
-console.log(monthToDays['2'](400) === 29);
+var isSunday = function(daysFromBeginningOfTime) {
+  // the beginning of time begins at Jan 1 1900, which was a Monday :)
+  var beginningOfTime = 0;
+  if(!((daysFromBeginningOfTime - beginningOfTime) % 7)) {
+    return true;
+  }
+};
 
+var sundays = function(startYear, endYear) {
+  var counter = 0;
+  var year = startYear;
+  var month = 1, dayInMonth = 1;
+  var daysInMonth = monthToDays[month](year);
+  var totalDays = 1;
+  while(year < endYear) {
+    for(var day = 1; day <= daysInYear(year); day++, dayInMonth++, totalDays++) {
+      if(dayInMonth === 1 && isSunday(totalDays)) {
+        counter++;
+      }
+      if(dayInMonth >= daysInMonth) {
+        dayInMonth = 0;
+        month++;
+        if(month > 12) {
+          year++;
+          month = 1;
+        }
+        daysInMonth = monthToDays[month](year);
+      }
 
+    }
+  }
+  return counter;
+}
+console.log(sundays(1900, 2001) - sundays(1900, 1901));
