096071acffe68b0fb474ad82c7cb50f38b374e50
Tessa Kelly <tessakkelly@gmail.com>
2015-06-13 16:27:02 -0700

    expand buildRange functionality

--- range.js
+++ range.js
@@ -1,12 +1,46 @@
-var buildRange = function(start, topNumber) {
-  var rangeNums = [];
-  if(topNumber === undefined) {
-    topNumber = start;
-    start = 1;
+var buildRange = function() {
+  // determine whether we want an object or an array
+  var args = [].slice.call(arguments);
+  var rangeNums, startNum, topNum;
+
+  if(args[1] === true || args[2] === true) {
+    // We want an object returned to us
+    rangeNums = {};
+
+    if(arguments.length === 2) {
+      // Start value defaults to 1
+      start = 1;
+      topNum = args[0];
+    } else {
+      // Start value provided
+      start = args[0];
+      topNum = args[1];
+    }
+
+    // build range object
+    for(; start <= topNum; start++) {
+      rangeNums[start] = true;
+    }
+
+  } else {
+    // We want an array returned to us
+    rangeNums = [];
+
+    if(arguments.length === 1) {
+      // Start value defaults to 1
+      start = 1;
+      topNum = args[0];
+    } else {
+      // Start value provided
+      start = args[0];
+      topNum = args[1];
+    }
+
+    for(; start <= topNum; start++) {
+      rangeNums.push(start);
+    }
   }
-  for(var i = start; i <= topNumber; i++) {
-    rangeNums.push(i);
-  }
+
   return rangeNums;
 }
 
