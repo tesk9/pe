3076e110d8bc1525eb1e3feead2c4c3883db1654
Tessa Kelly <tessakkelly@gmail.com>
2015-06-13 15:59:11 -0700

    add abundantNumberLister

--- nonAbundantSums.js
+++ nonAbundantSums.js
@@ -1,8 +1,9 @@
 /* a number is abundant if the sum of its proper divisors is greater than the number.
    it is deficient if the sum of its proper divisors is less than it
-   and n is perfect if the sum of n's proper divisors IS n */
+   and n is perfect if the sum of n's proper divisors IS n
 
-// Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers
+   Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers
+   Note that all integers greater than 28123 can be written as the sum of two abundant numbers. */
 
 var getFactors = require('./factorization.js');
 
@@ -13,5 +14,16 @@ var isAbundant = function(number) {
   return properDivisorSum > number;
 };
 
-console.log(isAbundant(4) === false); // true
-console.log(isAbundant(12) === true); // true
+var abundantNumberLister = function(topNum) {
+  var list = {}, num = 1;
+  while(num < topNum) {
+    if(isAbundant(num)) {
+      list[num] = true;
+    }
+    num++;
+  }
+  return list;
+}
+
+// console.log(isAbundant(4) === false); // true
+// console.log(isAbundant(12) === true); // true
