dd02cb86899e4ad3ee1851f0dc14e7b0e91a60b8
Tessa Kelly <tessakkelly@gmail.com>
2015-06-22 16:57:17 -0700

    solve truncatablePrimes
    This reverts commit 6ae1933d4c191aed1c9bd1d4ff0e3704f8b41dd4.

--- truncatablePrimes.js
+++ truncatablePrimes.js
@@ -1,16 +1,49 @@
 var primeSieve = require('./sieveOfErastothanes');
 
 var truncatablePrimes = function() {
-  
+  var truncatablePrimes = [];
+  var primeInd = 0;
+  var guess = 100000;
+  var primes = primeSieve(guess);
+
+  var primeLookup = {};
+  primes.forEach(function(prime) {
+    primeLookup[prime] = 1;
+  });
+
+  while(truncatablePrimes.length < 11) {
+    if(primeInd === primes.length) {
+      guess *= 2;
+      var newPrimes = primeSieve(guess, primes);
+      for(var i = primeInd; i < newPrimes.length; i++) {
+        primeLookup[newPrimes[i]] = 1;
+      }
+      primes.pop();
+      primes = primes.concat(newPrimes);
+    }
+    var prime = primes[primeInd];
+    var truncations = truncate(prime);
+    if(truncations.every(function(combo) {
+      return primeLookup[combo];
+    })) {
+      prime > 9 ? truncatablePrimes.push(prime) : "";
+    }
+    primeInd++;
+  };
+
+  return truncatablePrimes.reduce(function(a,b) {
+    return +(a) + +(b);
+  });
 };
 
 var truncate = function(input) {
   var combinations = [];
   var elements = ("" + input).split("");
   for(var i = 0; i < elements.length; i++) {
-    combinations.push(elements.slice(i));
-    combinations.push(elements.slice(0, i + 1));
+    combinations.push(elements.slice(i).join(""));
+    combinations.push(elements.slice(0, i + 1).join(""));
   }
   return combinations;
 };
 
+console.log(truncatablePrimes());
