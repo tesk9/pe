8cd81b8b12856b452269ae6289ec064e909e3a79
Tessa Kelly <tessakkelly@gmail.com>
2015-06-23 09:19:20 -0700

    add problem description for lexicographic permutations

---
+++ lexicographicPermutations.js
@@ -0,0 +1,7 @@
+/* 
+  The lexicographic permutations of 0, 1, and 2 are:
+    012 021 102 120 201 210
+
+  What is the millionth lexicographic permutation of the
+  digits 0, 1, 2, 3, 4, 5, 6, 7, 8, 9?
+*/
