4f6e8a35c0d2b52b4df01ae8bc9e36619ca56e75
Tessa Kelly <tessakkelly@gmail.com>
2015-07-08 10:32:45 -0700

    start numberSpiralDiagonals (passing example case)

--- numberSpiralDiagonals.js
+++ numberSpiralDiagonals.js
@@ -14,3 +14,25 @@ the diagonals is 101.
 What is the sum of the numbers on the diagonals
 in a 1001 by 1001 spiral formed in the same way?
 */
+
+var numberSpiralDiagonals = function(n) {
+  var step = 2;
+  var nums = [];
+  var current = 1;
+  var times = 1;
+  while(step <= n) {
+    nums.push(current);
+
+    if(times < 5) {
+      times++;
+    } else {
+      step += step;
+      times = 2;
+    }
+
+    current += step;
+  }
+  return nums.reduce(function(a, b) { return a + b; });
+};
+
+console.log(numberSpiralDiagonals(5));
