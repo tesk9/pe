fb6bc199687c23848ee3629a31db32b52a7b80ec
Tessa Kelly <tessakkelly@gmail.com>
2015-07-13 15:25:21 -0700

    add function producing string representation of Champernowne's constant to at least the nth digit, where n is passed as an argument

--- champerownesConstant.js
+++ champerownesConstant.js
@@ -11,3 +11,19 @@
 
   d1 × d10 × d100 × d1000 × d10000 × d100000 × d1000000
 */
+
+var champerowne = function(nthDigit) {
+  var currentInd = 1;
+  var currentPosInt = 1;
+  var arr = ["."];
+
+  while(currentInd <= nthDigit) {
+    currentInd += ("" + currentPosInt).length;
+    arr.push(currentPosInt)
+    currentPosInt++;
+  }
+
+  return arr.join("");
+};
+
+console.log(champerowne(12));
