753e9f0a2e15c46ef85289e14920bd0f7468456a
Tessa Kelly <tessakkelly@gmail.com>
2015-07-13 20:03:18 -0700

    add tighter upper bound and improve efficiency by replacing redudant factorial calculations with a lookup table

--- digitFactorials.js
+++ digitFactorials.js
@@ -3,13 +3,20 @@ to the sum of the factorial of their digits */
 
 var factorial = require('./factorial');
 
+// Create factorial lookup table
+var factorialTable = {};
+for(var i = 0; i < 10; i++) {
+  factorialTable[i] = factorial(i);
+}
+console.log(factorialTable);
+
 var digitsFactorialSum;
 var num = 3;
 var total = 0;
-while(num < 10000000) {
+while(num < factorialTable[9] * 7) {
   digitsFactorialSum = 0;
   ("" + num).split("").forEach(function(digit) {
-    digitsFactorialSum += factorial(digit);
+    digitsFactorialSum += factorialTable[digit];
   });
   if(num === digitsFactorialSum) {
     total += num;
