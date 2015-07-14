a7bee9140712ef2253a8bbc7df632887a6ea0e6b
Tessa Kelly <tessakkelly@gmail.com>
2015-07-13 22:47:02 -0700

    add rightAngleTriangles function

--- integerRightTriangles.js
+++ integerRightTriangles.js
@@ -6,3 +6,42 @@
 
   For which value of p ≤ 1000, is the number of solutions maximised?
 */
+
+/* 
+  Returns all right angle triangle side length possibilities for the
+  given perimeter
+*/
+var rightAngleTriangles = function(p) {
+  var a = 1, b = 1, c = 3;
+  var solutions = [];
+  var isSolution = combinedEquations(p);
+
+  while(b < p / 2) {
+    while(a < b) {
+      c = Math.sqrt(a*a + b*b);
+      console.log(a,b)
+      if(isSolution(a, b)) {
+        solutions.push([a, b, c]);
+      }
+      a++;
+    }
+    a = 1;
+    b++;
+  }
+
+  return solutions;
+};
+
+/*
+  using a^2 + b^2 = c^2 and a + b + c = p, returns a function that evaluates
+  whether some a,b combination is a valid solution
+*/
+function combinedEquations (p) {
+  return function(a,b) {
+    return a + b + Math.sqrt(a * a + b * b) === p;
+  }
+};
+
+// console.log(rightAngleTriangles(3+4+5));
+console.log(rightAngleTriangles(120));
+
