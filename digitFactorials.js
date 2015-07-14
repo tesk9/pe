a6be73976867b39e9e59dc4bf8ab21c39c924e71
Tessa Kelly <tessakkelly@gmail.com>
2015-07-13 19:54:17 -0700

    solve digitFactorials (brute force--why is the upper limit what it is?)

--- digitFactorials.js
+++ digitFactorials.js
@@ -1,2 +1,20 @@
 /* Find the sum of all numbers which are equal 
 to the sum of the factorial of their digits */
+
+var factorial = require('./factorial');
+
+var digitsFactorialSum;
+var num = 3;
+var total = 0;
+while(num < 10000000) {
+  digitsFactorialSum = 0;
+  ("" + num).split("").forEach(function(digit) {
+    digitsFactorialSum += factorial(digit);
+  });
+  if(num === digitsFactorialSum) {
+    total += num;
+  }
+  num++;
+}
+
+console.log(total);
