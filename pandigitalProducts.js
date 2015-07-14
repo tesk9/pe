11b2ea65bd06d5073699dd4d801c20c94cd59737
Tessa Kelly <tessakkelly@gmail.com>
2015-07-13 17:21:51 -0700

    add work on pandigitalProducts--not getting correct answer

--- pandigitalProducts.js
+++ pandigitalProducts.js
@@ -25,7 +25,72 @@ var isPandigital = function(num) {
   return true;
 };
 
-console.log(isPandigital(15234) === true);
-console.log(isPandigital(15534) === false);
-console.log(isPandigital(1) === true)
+var findPandigitalProducts = function() {
+  var products = {};
+  for(var multiplicand = 1; multiplicand < 8000; multiplicand++) {
+    var multiplicandLookup = objectifyIfNoRepetitions(multiplicand);
+    
+    // If there are no repetitions, enter the while-loop
+    if(multiplicandLookup) {
+      var incrementer = incrementWithoutReps(multiplicandLookup, multiplicand);
+      var multiplier = 1;
 
+      while(multiplier < multiplicand) {
+        var product = multiplicand * multiplier;
+
+        if(isPandigital("" + multiplicand + multiplier + product)) {
+          products[product] = 1;
+        }
+        multiplier = incrementer(multiplier);
+      }
+
+    }
+
+  }
+
+  return Object.keys(products).reduce(function(a,b) {
+    return a + +(b);
+  }, 0);
+};
+
+var objectifyIfNoRepetitions = function(number) {
+  var store = {};
+  var num = ("" + number).split("");
+  for(var i = 0; i < num.length; i++) {
+    if(store[num[i]]) {
+      return false;
+    }
+    store[num[i]] = 1;
+  }
+  if(store[0]) {
+    return false;
+  }
+  return store;
+};
+
+var incrementWithoutReps = function(multiplicandStore, multiplicand) {
+  return function recurse(current) {
+    current++;
+    var currentStore = {};
+    var currentArr = ("" + current).split("");
+    for(var i = 0; i < currentArr.length; i++) {
+      /* If there are repititions when comparing 
+         with the multiplicand store or
+         with the currentStore, increment current */
+      if(currentStore[currentArr[i]] || multiplicandStore[currentArr[i]]) {
+        if(current > multiplicand) {
+          return multiplicand++;
+        }
+        return recurse(current);
+      }
+      currentStore[currentArr[i]] = 1;
+    }
+    if(currentStore[0]) {
+      return recurse(current);
+    }
+    return current;
+  }
+};
+
+
+console.log(findPandigitalProducts());
