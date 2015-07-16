368233d2ab1c87ab651ad87e19b4da762b74a475
Tessa Kelly <tessakkelly@gmail.com>
2015-07-16 08:29:47 -0600

    solve goldbach's other conjecture

--- goldbachsOtherConjecture.js
+++ goldbachsOtherConjecture.js
@@ -15,7 +15,11 @@
   sum of a prime and twice a square?
 */
 
-var primes = require('./sieveOfErastothanes');
+var primes = require('./sieveOfErastothanes')(10000);
+var primeLookup = {};
+primes.forEach(function(val) { 
+  primeLookup[val] = true; 
+});
 
 var squareLookup = (function() {
   var store = {};
@@ -29,4 +33,32 @@ var squareLookup = (function() {
     return !!store[number];
   };
 })();
+
+
+var checkConjecture = function(number) {
+  for(var i = 0; i < primes.length; i++) {
+    if(primes[i] > number) {
+      break;
+    }
+    if(squareLookup((number - primes[i]) / 2)) {
+      return true;
+    }
+  }
+}
+
+// Finding the smallest odd composite that cannot be written as the 
+// sum of a prime and twice a square:
+var findFailingComposite = function() {
+  var currentOddComposite = 3;
+  while(true) {
+    if(!primeLookup[currentOddComposite] && !checkConjecture(currentOddComposite)) {
+      return currentOddComposite;
+    }
+
+    currentOddComposite += 2;
+  }
+};
+
+console.log(findFailingComposite());
+
 
