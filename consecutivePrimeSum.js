7e65c1ad5d4c275523f4ebc481c126d6e7a7d917
Tessa Kelly <tessakkelly@gmail.com>
2015-07-19 11:16:19 -0600

    add function that finds the longest consecutive prime under a thousand.

--- consecutivePrimeSum.js
+++ consecutivePrimeSum.js
@@ -11,3 +11,38 @@
   Which prime, below one-million, can be written as the sum 
   of the most consecutive primes?
 */
+
+var primes = require('./sieveOfErastothanes')(1000);
+var primeLookup = {};
+
+primes.forEach(function(val) {
+  primeLookup[val] = 1;
+});
+
+var longestConsecutivePrimeSum = function(upperBound) {
+  var sums = [1].concat(primes.slice());
+  var passCounter = 0;
+  var longestSum = 0;
+  var longestLength = 0;
+  var newSum;
+
+  while(passCounter < primes.length) {
+    for(var i = 0; i < sums.length - 1; i++) {
+      newSum = sums[i] + primes[passCounter + i];
+      sums[i] = newSum;
+
+      // Check to see if sum is prime
+      if(primeLookup[newSum] && passCounter > longestLength) {
+        longestSum = newSum;
+        longestLength = passCounter;
+      }
+
+    }
+    sums.pop();
+    passCounter++;
+  }
+
+  return longestSum;
+};
+
+console.log(longestConsecutivePrimeSum(10))
