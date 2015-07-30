68f2a15d350d2cfe41cdfc0dc6e9f1a22525c632
Tessa Kelly <tessakkelly@gmail.com>
2015-07-29 22:32:50 -0700

    add sum function to helpers

--- helpers.js
+++ helpers.js
@@ -7,8 +7,22 @@ var objectify = function(arr, def) {
   return obj;
 };
 
-module.exports = (function() {
-  return {
-    objectify: objectify
-  };
-})();
+// Sums over object keys or over an array
+var sum = function(obj) {
+  var sum = 0;
+  if(Array.isArray(obj)) {
+    for(var i = 0; i < obj.length; i++) {
+      sum += +(obj[i]);
+    }
+  } else {
+    for(var key in obj) {
+      sum += +(key);
+    }
+  }
+  return sum;
+};
+
+module.exports = {
+  objectify: objectify,
+  sum: sum
+};
